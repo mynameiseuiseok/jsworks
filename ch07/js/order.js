d0c2e15701ebaa506ad38e834e23135017dc10ab
mynameiseuiseok <sunges1014@naver.com>
2023-04-06 18:18:10 +0900

    팝업, 메인

--- ch07/js/order.js
+++ ch07/js/order.js
@@ -1,26 +1,30 @@
 //체크 박스 속성에 이벤트 처리하기
-let check = document.querySelector("#shippingInfo");
+window.onload = function(){ //익명 실행 함수
+    let check = document.querySelector("#shippingInfo");
 
-//체크 이벤트 구현
-check.addEventListener("click", checkBox);
-
-let billingName = document.getElementById("billingName");
-let billingTel = document.getElementById("billingTel");
-let billingAddr = document.getElementById("billingAddr");
-
-let shippingName = document.getElementById("shippingName");
-let shippingTel = document.getElementById("shippingTel");
-let shippingAddr = document.getElementById("shippingAddr");
-
-function checkBox(){
-    if(check.checked == true){
-        shippingName.value = billingName.value;
-        shippingTel.value = billingTel.value;
-        shippingAddr.value = billingAddr.value;
-    }
-    else{
-        shippingName.value == "";
-        shippingTel.value == "";
-        shippingAddr.value == "";
+    //체크 이벤트 구현
+    check.addEventListener("click", checkBox);
+    
+    let billingName = document.getElementById("billingName");
+    let billingTel = document.getElementById("billingTel");
+    let billingAddr = document.getElementById("billingAddr");
+    
+    let shippingName = document.getElementById("shippingName");
+    let shippingTel = document.getElementById("shippingTel");
+    let shippingAddr = document.getElementById("shippingAddr");
+    
+    function checkBox(){
+        //체크 시 기본정보 값을 배송 정보에 저장
+        if(check.checked == true){
+            shippingName.value = billingName.value;
+            shippingTel.value = billingTel.value;
+            shippingAddr.value = billingAddr.value;
+        }
+        else{
+            //체크 해제 시 check.checked == false 입력상자 공백 초기화
+            shippingName.value == "";
+            shippingTel.value == "";
+            shippingAddr.value == "";
+        }
     }
 }
