00db32839bf040e8234391aed35f69f49e3a3b4a
mynameiseuiseok <sunges1014@naver.com>
2023-04-11 18:08:17 +0900

    제이쿼리

---
+++ ch04/js/event_jq.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btn_show").click(function(){
+        $("#detail").show();
+        $(this).hide();
+    });
+    $("#btn_hide").click(function(){
+        $("#detail").hide();
+        $("#btn_show").show();
+    });
+    
+});
