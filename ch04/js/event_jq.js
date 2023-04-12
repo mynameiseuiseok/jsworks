45eff574d7aae81898f0444931c79864fb6b3e9f
mynameiseuiseok <sunges1014@naver.com>
2023-04-12 18:13:18 +0900

    쿼리

--- ch04/js/event_jq.js
+++ ch04/js/event_jq.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
     $("#btn_show").click(function(){
         $("#detail").show();
-        $(this).hide();
+        $(this).hide(); //클릭한 버튼 자신
     });
     $("#btn_hide").click(function(){
         $("#detail").hide();
         $("#btn_show").show();
     });
-    
 });
