688f84d9aee7b49be11bbf9dc2e7ddb520730f37
mynameiseuiseok <sunges1014@naver.com>
2023-07-14 17:26:25 +0900

    20230714 jsworks

---
+++ header/js/main.js
@@ -0,0 +1,22 @@
+// 자바스크립트 구현
+let toggleBtn = document.querySelector(".navbar_toggleBtn")
+let menu = document.querySelector(".navbar_menu");
+/* 
+toggleBtn.addEventListener('click', function(){
+    menu.classList.toggle('active');  // active 스타일 추가 및 
+}); */
+
+// 제이쿼리 구현
+$(function(){
+    // 메뉴바 클릭했을 때
+    $(".navbar_toggleBtn").click(function(){
+        $(".navbar_menu").toggle("active");
+    });
+    
+    // 윈도우 크기를 조절하는 기능
+    $(window).resize(function(){
+        if(parseInt($('nav').css('width')) > 768){  // 정수로 변환 크기 비교
+            $('.navbar_menu').css('display', 'flex');
+        }
+    })
+});
